b644d233717b74a3328bd80d4e3c30e21a32c311
fudanzz <fudanzz@gmail.com>
2016-09-30 22:19:00 +0800

    update

--- scope.js
+++ scope.js
@@ -1,3 +1,5 @@
+//lexical scope
+
 var foo = 'foo';
 
 (function () {
@@ -12,3 +14,38 @@ console.log(foo);
     var foo = bar;
     console.log(foo);
 })(foo);
+
+
+
+//block level scope by using let
+function foo1() {
+
+    var bar = "bar";
+
+    for (let i = 0; i < bar.length; i++) {
+        console.log(bar.charAt(i));
+    }
+
+    //console.log(i);
+}
+
+foo1();
+
+
+function demo() {
+    console.log(bar);
+}
+
+function barz() {
+    var bar = 'dd';
+    //demo();
+}
+
+barz();
+
+a;
+b;
+var a = b;
+var b = 2;
+console.log(a);
+console.log(b);
