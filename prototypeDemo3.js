8e4c643750f66474a0f8bd05813d0bc43d52880d
fudanzz <fudanzz@gmail.com>
2016-10-07 14:28:13 +0800

    update

--- prototypeDemo3.js
+++ prototypeDemo3.js
@@ -41,3 +41,39 @@ for (arrt in circle) {
 console.log(circle.constructor.prototype.desc);
 
 console.log(Array.constructor);
+
+var test = {
+    name: "phil"
+};
+
+console.log(typeof test.name);
+
+
+console.log(typeof test.constructor);
+
+var array = [3, 4];
+
+
+function add(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw {
+            name: 'TypeError',
+            message: 'add need number'
+        }
+    }
+    return a + b;
+}
+
+console.log(add.apply(null, array));
+
+
+Function.prototype.method = function (name, func) {
+    this.prototype[name] = func;
+    return this;
+}
+
+Number.method('integer', function () {
+    return Math[this < 0 ? 'ceil' : 'floor'](this);
+});
+
+console.log((3.6).integer());
